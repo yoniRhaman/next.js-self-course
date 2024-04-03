fe71f2fb8b29e1bb346c24464d5819bd548a5e89
yonatan.r <y.rahman770770@gmail.com>
2024-04-03 12:34:34 +0300

    Dynamic Routes

---
+++ hello-world/src/app/products/[productId]/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductDetails({params}:{params:{productId:string}}) {
+  return <h1>Details about Product {params.productId}</h1>;
+}
---
+++ hello-world/src/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function ProductList() {
+  return (
+    <div>
+      <h1>product List</h1>
+      <h2>product 1</h2>
+      <h2>product 2</h2>
+      <h2>product 3</h2>
+    </div>
+  );
+}
+ 
