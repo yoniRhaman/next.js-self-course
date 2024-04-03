74c3a4df1a1f1a369a6d90493c61fc4b942a22db
yonatan.r <y.rahman770770@gmail.com>
2024-04-03 14:36:52 +0300

    Not Found Pages

---
+++ hello-world/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function Notfound() {
+  return (
+    <div>
+      <h2>Page Not found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  );
+}
---
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function Notfound() {
+  return (
+    <div>
+      <h2>Reviews Not found</h2>
+    </div>
+  );
+}
